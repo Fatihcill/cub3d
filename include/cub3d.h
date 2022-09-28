df378007099d7b0082ffd577133d80b108f9c2bf
Fatih Cil <fcil@k2m15s08.42kocaeli.com.tr>
2022-09-28 14:50:20 +0300

    start

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: fcil <fcil@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 20:51:11 by fcil              #+#    #+#             */
-/*   Updated: 2022/07/10 21:29:25 by fcil             ###   ########.fr       */
+/*   Updated: 2022/09/28 13:12:23 by fcil             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,17 @@
 
 # define SIZE_X 1000.0
 # define SIZE_Y 1000.0
+
+typedef struct s_mlx
+{
+	void			*ptr;
+}				t_mlx;
+
+typedef struct s_win
+{
+	void			*ptr;
+	int				x;
+	int				y;
+}				t_win;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,18 +6,33 @@
 /*   By: fcil <fcil@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 20:50:50 by fcil              #+#    #+#             */
-/*   Updated: 2022/07/10 21:44:00 by fcil             ###   ########.fr       */
+/*   Updated: 2022/09/28 13:13:50 by fcil             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	main(void)
+int	ft_namecheck(char *arg, char *ext)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i] != '\0')
+		i++;
+	if ((i > 4 && arg[i - 1] == ext[2] && arg[i - 2] == ext[1])
+		&& (arg[i - 3] == ext[0] && arg[i - 4] == '.'))
+		return (1);
+	return (0);
+}
+
+int	main(int ac, char **av)
 {
 	void	*mlx;
 	void	*mlx_win;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, SIZE_X, SIZE_Y, "cub3D");
-	mlx_loop(mlx);
+	if (ac == 2 && ft_namecheck(av[1], "cub"))
+		ft_init(av[1]);
+	else
+		write(2, "Error : Invalid arguments\n", 26);
+	return (0);
 }
